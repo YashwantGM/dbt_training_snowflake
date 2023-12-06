d0b8fedc9921f4708ba4ddeae2948c45269837cb
yashgm96@gmail.com <yashgm96@gmail.com>
2023-12-06 13:01:31 +0000

    First and second model - training and experiments

--- models/example/example_subfolder/my_first_dbt_model.sql
+++ models/example/example_subfolder/my_first_dbt_model.sql
@@ -5,9 +5,9 @@
     This will override configurations stated in dbt_project.yml
 
     Try changing "table" to "view" below
+ {?{ config(materialized='view') }}?
 */
 
-{{ config(materialized='table') }}
 
 with source_data as (
 
@@ -24,4 +24,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+ where id is not null
